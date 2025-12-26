dfd5b40ee28e3f953af484e24ddd63c8e7b403b2
swg00 <maksimlatypov420@gmail.com>
2025-12-26 14:19:10 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -37,9 +37,5 @@ async def comment_under_post(message: Message):
         reply_to_message_id=message.message_id
     )
 
-async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
-
 if __name__ == "__main__":
     asyncio.run(main())
